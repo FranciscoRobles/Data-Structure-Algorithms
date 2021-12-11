08d9a1ea53dc476f99ab9d9acbf1b6568b438f84
JOSE FRANCISCO ROBLES SAPIEN <jrobles@ibm.com>
2021-12-10 23:59:12 -0600

    Section 2 - Insertion Sort

---
+++ BasicSorting/src/src/InsertionSort.java
@@ -0,0 +1,28 @@
+package src;
+
+public class InsertionSort {
+
+	/*
+	 * Insertion Sort: O(n2)
+	 * Sorting applied when adding/accessing new values to/of the structure; 
+	 * 	the structure is already sorted, so we just compare with the next/top most element.
+	 */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] list = {5, 2, 8, 12, 7};
+		for(int i = 1; i<list.length; i++) {
+			int current = list[i];
+			int j = i-1;
+			while(j >= 0 && list[j]>current) {
+				list[j+1] = list[j];
+				j--;
+			}
+			list[j+1] = current;
+		}
+		for(int i = 0; i < list.length; i++) {
+			System.out.print(list[i] + " ");
+		}
+	}
+
+}
