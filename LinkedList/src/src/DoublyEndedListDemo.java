b6945ec0d9712a245f5b2d6ad50c1f8e57698062
JOSE FRANCISCO ROBLES SAPIEN <jrobles@ibm.com>
2021-12-11 23:08:06 -0600

    Section 4 - Doubly Ended List

---
+++ LinkedList/src/src/DoublyEndedListDemo.java
@@ -0,0 +1,18 @@
+package src;
+
+public class DoublyEndedListDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DoublyEndedList list = new DoublyEndedList();
+		
+		list.insertAtTail(6578);
+		list.insertAtTail(90);
+		list.insertAtTail(6);
+		list.insertAtTail(100);
+		list.insertAtTail(1);
+		
+		System.out.println(list);
+	}
+
+}
