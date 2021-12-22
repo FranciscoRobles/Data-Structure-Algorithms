9e275644b6bf8e3837d734696435fdb306f132ee
JOSE FRANCISCO ROBLES SAPIEN <jrobles@ibm.com>
2021-12-22 09:56:10 -0600

    Section 5 - Stack

---
+++ Stacks&Queue/src/src/Stack.java
@@ -0,0 +1,39 @@
+package src;
+
+public class Stack {
+	private static int top = -1;
+	static int maxSize = 9;
+	static int[] array = new int[maxSize];
+	
+	public static void push(int number) {
+		top++;
+		array[top] = number;
+	}
+	
+	public static int peek() {
+		return array[top];
+	}
+	
+	public static int pop() {
+		int popped = array[top];
+		array[top] = 0;
+		top--;
+		return popped;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		push(12);
+		push(5);
+		for(int i = 0; i<array.length; i++) {
+			System.out.print(array[i] + ",");
+		}
+		System.out.println();
+		System.out.println("Peek: " + peek());
+		System.out.println("Pop:" + pop());
+		for(int i = 0; i<array.length; i++) {
+			System.out.print(array[i] + ",");
+		}
+	}
+
+}
