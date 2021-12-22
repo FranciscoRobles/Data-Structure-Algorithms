57546aff3d45e2d5a344bb2d423e5da1b0cca9cd
JOSE FRANCISCO ROBLES SAPIEN <jrobles@ibm.com>
2021-12-22 15:34:21 -0600

    Section 6 - Tail Recursion

--- Recursion/src/src/Factorial.java
+++ Recursion/src/src/Factorial.java
@@ -2,6 +2,12 @@ package src;
 
 public class Factorial {
 	
+	//We have an accumulator (result) which holde the result of the previous calculation, avoiding Stack Overflow
+	public static int tailFactorial(int n, int result) {
+		if(n == 0) return result;
+		return tailFactorial(n-1, n*result);
+	}
+	
 	public static int factorial(int n) {
 		if(n == 0) {
 			return 1;
@@ -12,6 +18,7 @@ public class Factorial {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(factorial(3));
+		System.out.println(tailFactorial(4, 1));
 
 	}
 
