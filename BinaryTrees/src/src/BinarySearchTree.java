daddb12d4270198ce985d881d75d0e03cc0b640c
JOSE FRANCISCO ROBLES SAPIEN <jrobles@ibm.com>
2021-12-23 10:59:37 -0600

    Section 7 - Soft Delete

--- BinaryTrees/src/src/BinarySearchTree.java
+++ BinaryTrees/src/src/BinarySearchTree.java
@@ -20,44 +20,75 @@ public class BinarySearchTree {
 		return null;
 	}
 	
-	public void deleteNode(int data) {
-		TreeNode current = this.rootNode;
-		TreeNode parent = this.rootNode;
-		boolean isLeftChild = false;
-		
-		if(current == null) {
-			return;
-		}
-		while(current != null && current.getData() != data) {
-			parent = current;
-			if(data > current.getData()) {
-				current = current.getRightChild();
-				isLeftChild = false;
-			}
-			else {
-				current = current.getLeftChild();
-				isLeftChild = true;
-			}
-		}
-		
-		if(current == null) {
-			return;
-		}
-		if(current.getLeftChild() == null && current.getRightChild() == null) {
-			if(current == this.rootNode) {
-				this.rootNode = null;
-			}
-			else {
-				if(isLeftChild) {
-					parent.setLeftChild(null);
-				}
-				else {
-					parent.setRightChild(null);
-				}
-			}
-		}
-		
+	//Soft Delete
+	public void delete(int data) {
+		TreeNode toDelete = findNode(data);
+		toDelete.delete();
 	}
+	
+//	public void deleteNode(int data) {
+//		TreeNode current = this.rootNode;
+//		TreeNode parent = this.rootNode;
+//		boolean isLeftChild = false;
+//		
+//		if(current == null) {
+//			return;
+//		}
+//		while(current != null && current.getData() != data) {
+//			parent = current;
+//			if(data > current.getData()) {
+//				current = current.getRightChild();
+//				isLeftChild = false;
+//			}
+//			else {
+//				current = current.getLeftChild();
+//				isLeftChild = true;
+//			}
+//		}
+//		
+//		if(current == null) {
+//			return;
+//		}
+//		//Case 1: Node is a Leaf
+//		if(current.getLeftChild() == null && current.getRightChild() == null) {
+//			if(current == this.rootNode) {
+//				this.rootNode = null;
+//			}
+//			else {
+//				if(isLeftChild) {
+//					parent.setLeftChild(null);
+//				}
+//				else {
+//					parent.setRightChild(null);
+//				}
+//			}
+//		}
+//		//Case 2: Node has 1 child
+//		else if(current.getRightChild() == null) {
+//			if(current == this.rootNode) {
+//				this.rootNode = current.getLeftChild();
+//			}
+//			else if(isLeftChild){
+//				parent.setLeftChild(current.getLeftChild());
+//			}
+//			else {
+//				parent.setRightChild(current.getLeftChild());
+//			}
+//		}
+//		else if(current.getLeftChild() == null) {
+//			if(current == this.rootNode) {
+//				this.rootNode = current.getRightChild();
+//			}
+//			else if(isLeftChild){
+//				parent.setLeftChild(current.getRightChild());
+//			}
+//			else {
+//				parent.setRightChild(current.getRightChild());
+//			}
+//		}
+//		//Case 3: Node has 2 childs - It's too complicated, so we will use Soft Delete
+//		
+//	}
 
 	public static void main(String[] args) {
 		System.out.println("The element doesn't exist");
