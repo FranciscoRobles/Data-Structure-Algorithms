77e8e7439cad6f054ac6b4a19c3225d592685c75
JOSE FRANCISCO ROBLES SAPIEN <jrobles@ibm.com>
2021-12-22 18:18:07 -0600

    Section 6 - Merge Sort

---
+++ Recursion/src/src/MergeSort.java
@@ -0,0 +1,52 @@
+package src;
+import java.lang.Math;
+public class MergeSort {
+
+	public static void merge(int[] a, int[] l, int[] r, int left, int right) {
+		int i = 0, j = 0, k = 0;
+		while (i < left && j < right) {
+			if (l[i] <= r[j]) {
+				a[k++] = l[i++];
+			}
+			else {
+			            a[k++] = r[j++];
+			        }
+			    }
+			    while (i < left) {
+			        a[k++] = l[i++];
+			    }
+			    while (j < right) {
+			        a[k++] = r[j++];
+			    }
+			}
+	
+	public static void mergeSort(int[] a, int n) {
+	    if (n < 2) {
+	        return;
+	    }
+	    int mid = n / 2;
+	    int[] l = new int[mid];
+	    int[] r = new int[n - mid];
+
+	    for (int i = 0; i < mid; i++) {
+	        l[i] = a[i];
+	    }
+	    for (int i = mid; i < n; i++) {
+	        r[i - mid] = a[i];
+	    }
+	    mergeSort(l, mid);
+	    mergeSort(r, n - mid);
+
+	    merge(a, l, r, mid, n - mid);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array = {23, 5, 2, 8, 12, 7, 16, 9};
+		mergeSort(array, array.length);
+		for(int i = 0; i<array.length; i++) {
+			System.out.print(array[i] + ", ");
+		}
+	}
+
+}
